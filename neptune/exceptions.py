9293b32b4beebeb554c57993f2a6aae0051863eb
Dominik Jastrzębski <dominik.jastrzebski@neptune.ml>
2019-02-20 15:30:07 +0100

    Change Not Found exceptions base classes

--- neptune/exceptions.py
+++ neptune/exceptions.py
@@ -44,17 +44,17 @@ class InvalidApiKey(NeptuneApiException):
         super(InvalidApiKey, self).__init__('The provided API key is invalid.')
 
 
-class OrganizationNotFound(Exception):
+class OrganizationNotFound(NeptuneApiException):
     def __init__(self, organization_name):
         super(OrganizationNotFound, self).__init__("Organization '{}' not found.".format(organization_name))
 
 
-class ProjectNotFound(Exception):
+class ProjectNotFound(NeptuneApiException):
     def __init__(self, project_identifier):
         super(ProjectNotFound, self).__init__("Project '{}' not found.".format(project_identifier))
 
 
-class ExperimentNotFound(Exception):
+class ExperimentNotFound(NeptuneApiException):
     def __init__(self, experiment_short_id, project_qualified_name):
         super(ExperimentNotFound, self).__init__("Experiment '{exp}' not found in '{project}'.".format(
             exp=experiment_short_id, project=project_qualified_name))
