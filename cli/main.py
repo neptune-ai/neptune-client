f15f656dfb666cc8d579d8e09a21f3a9df9be1b3
lukasz.walkiewicz <lukasz.walkiewicz@neptune.ml>
2019-02-25 14:33:27 +0100

    Add CLI (#27)

---
+++ cli/main.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2019, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import click
+
+
+@click.command()
+@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
+@click.argument('name', default='world', required=False)
+def main(name, as_cowboy):
+    greet = 'Howdy' if as_cowboy else 'Hello'
+    click.echo('{0}, {1}.'.format(greet, name))
