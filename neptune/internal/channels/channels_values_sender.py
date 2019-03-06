d851e4f07bc0c9b7c262d9f29e57b17239ee630c
Łukasz Walkiewicz <lukasz.walkiewicz@neptune.ml>
2019-03-06 18:41:26 +0100

    Improve channels values sending thread
      * Sleep appropriate time when values queue was empty
      * Stop gracefully on `join()`
      * Log errors using logging module (on DEBUG level)

--- neptune/internal/channels/channels_values_sender.py
+++ neptune/internal/channels/channels_values_sender.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import logging
 import threading
 import time
 from collections import namedtuple
@@ -20,9 +21,11 @@ from itertools import groupby
 
 from future.moves import queue
 
-from neptune.api_exceptions import NeptuneApiException
+from neptune.api_exceptions import NeptuneApiException, ChannelsValuesSendBatchError
 from neptune.internal.channels.channels import ChannelValue, ChannelIdWithValues
 from neptune.internal.threads.neptune_thread import NeptuneThread
+
+log = logging.getLogger(__name__)
 
 
 class ChannelsValuesSender(object):
@@ -76,27 +79,24 @@ class ChannelsValuesSendingThread(NeptuneThread):
 
     def run(self):
         sleep_time = self._SLEEP_TIME
-        while not self.is_interrupted():
+        while not self.is_interrupted() or not self._values_queue.empty():
             sleep_start = time.time()
             try:
                 self._values_batch.append(self._values_queue.get(timeout=sleep_time))
                 self._values_queue.task_done()
-                sleep_time = time.time() - sleep_start
+                sleep_time -= time.time() - sleep_start
             except queue.Empty:
                 sleep_time = 0
 
-            if sleep_time <= 0 or len(self._values_batch) > self._MAX_VALUES_BATCH_LENGTH:
+            if sleep_time <= 0 or len(self._values_batch) >= self._MAX_VALUES_BATCH_LENGTH:
                 self._process_batch()
                 sleep_time = self._SLEEP_TIME
 
-        self.join()
+        self._process_batch()
 
     def join(self, timeout=None):
         self.interrupt()
-        while not self._values_queue.empty():
-            self._values_batch.append(self._values_queue.get())
-            self._values_queue.task_done()
-        self._process_batch()
+        super(ChannelsValuesSendingThread, self).join(timeout)
 
     def _process_batch(self):
         send_start = time.time()
@@ -128,7 +128,8 @@ class ChannelsValuesSendingThread(NeptuneThread):
         # pylint: disable=protected-access
         try:
             self._experiment._send_channels_values(channels_with_values)
-        except NeptuneApiException:
-            pass
-        except IOError:
-            pass
+        except ChannelsValuesSendBatchError as e:
+            log.debug(e)
+        except (NeptuneApiException, IOError) as e:
+            log.debug("Received batch errors sending channels' values to experiment {}. "
+                      "Skipping {} values. Cause: ".format(self._experiment.id, len(queued_channels_values)) + e)
