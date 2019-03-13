add18f81134f9a3013563c8886905cc0d40a78c3
Adam Nieżurawski <5409107+aniezurawski@users.noreply.github.com>
2019-03-13 12:34:07 +0100

    Add cli supporting custom pluginable commands

--- setup.py
+++ setup.py
@@ -28,10 +28,36 @@ def main():
             long_description='Neptune Client',
             license='Apache License 2.0',
             install_requires=requirements,
-            packages=find_packages(include=['neptune*']),
+            packages=find_packages(),
             cmdclass={
                 'git_version': git_version.GitVersion,
-            }
+            },
+            entry_points={
+                'console_scripts': [
+                    'neptune = cli.main:main',
+                ],
+            },
+            classifiers=[
+                # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+                # 'Development Status :: 1 - Planning',
+                # 'Development Status :: 2 - Pre-Alpha',
+                # 'Development Status :: 3 - Alpha',
+                'Development Status :: 4 - Beta',
+                # 'Development Status :: 5 - Production/Stable',
+                # 'Development Status :: 6 - Mature',
+                # 'Development Status :: 7 - Inactive',
+                'Environment :: Console',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: Apache Software License',
+                'Operating System :: POSIX',
+                'Operating System :: MacOS',
+                'Operating System :: Unix',
+                'Operating System :: Microsoft :: Windows',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 3',
+                'Topic :: Software Development :: Libraries :: Python Modules',
+            ]
         )
 
 
