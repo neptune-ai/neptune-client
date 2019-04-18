63920686ea89f1a141d23d4f2929fac96f5983e4
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2019-04-18 16:10:37 +0200

    configuring (2)

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,9 @@ extensions = ['recommonmark']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The master toctree document.
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
@@ -53,4 +56,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_theme_path = ["_themes", ]
