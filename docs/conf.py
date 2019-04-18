690c28564680543e792caa703af9778ba56f6b53
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2019-04-18 15:56:08 +0200

    configuring

--- docs/conf.py
+++ docs/conf.py
@@ -21,14 +21,18 @@ project = 'neptune-client-docs'
 copyright = '2019, team neptune.ml'
 author = 'team neptune.ml'
 
+# The short X.Y version
+version = '0.2'
+# The full version, including alpha/beta/rc tags
+release = '0.2.9'
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
