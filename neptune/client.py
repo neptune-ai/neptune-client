c7dcc7a8ad371f361cbc636dce50c4b7744a9da5
Piotr Łusakowski <piotr.lusakowskI@neptune.ml>
2019-05-16 09:59:56 +0200

    Replace iteritems() with items() for compatibility with py3 (#99)

--- neptune/client.py
+++ neptune/client.py
@@ -787,10 +787,10 @@ class Client(object):
     def _upload_raw_data(self, api_method, data, headers, path_params, query_params):
         url = self.api_address + api_method.operation.path_name + "?"
 
-        for key, val in path_params.iteritems():
+        for key, val in path_params.items():
             url = url.replace("{" + key + "}", val)
 
-        for key, val in query_params.iteritems():
+        for key, val in query_params.items():
             url = url + key + "=" + val + "&"
 
         session = self._http_client.session
