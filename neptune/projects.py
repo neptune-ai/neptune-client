4f4eb21239416245f6f645d9eab141da9e13f762
Łukasz Walkiewicz <lukasz.walkiewicz@neptune.ml>
2019-07-01 12:34:31 +0200

    Add docstrings for `create/get_notebook` in `projects.py`
    Signed-off-by: Łukasz Walkiewicz <lukasz.walkiewicz@neptune.ml>

--- neptune/projects.py
+++ neptune/projects.py
@@ -14,9 +14,8 @@
 # limitations under the License.
 #
 
-from platform import node as get_hostname
-
 import os
+from platform import node as get_hostname
 
 import click
 import pandas as pd
@@ -417,15 +416,47 @@ class Project(object):
         )
 
     def create_notebook(self):
-        notebook = self.client.create_notebook(self)
-        return notebook
+        """Create a new notebook in Neptune.
+
+        Returns:
+            `neptune.notebook.Notebook` object.
+
+        Examples:
+            Instantiate a session and fetch a project.
+
+            >>> import neptune
+            >>> project = neptune.init()
+
+            Create a notebook.
+
+            >>> notebook = project.create_notebook()
+            """
+        return self.client.create_notebook(self)
 
     def get_notebook(self, notebook_id):
+        """Get a notebook with given id.
+
+        Returns:
+            `neptune.notebook.Notebook` object.
+
+        Examples:
+            Instantiate a session and fetch a project.
+
+            >>> import neptune
+            >>> project = neptune.init()
+
+            Get a notebook.
+
+            >>> notebook = project.get_notebook('d1c1b494-0620-4e54-93d5-29f4e848a51a')
+            """
         return self.client.get_notebook(project=self, notebook_id=notebook_id)
 
     @property
     def full_id(self):
-        """Creates a full project id by combining the namespace and project name.
+        """Create a full project id by combining project's namespace and name.
+
+        Returns:
+            str: Project's qualified name.
         """
         return '{}/{}'.format(self.namespace, self.name)
 
