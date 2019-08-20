cd0a7303c0be41b9e91479bbd53ca9516a15a470
Hubert Jaworski <hubert.jaworski@neptune.ml>
2019-08-20 09:26:36 +0200

    Review changes

--- neptune/internal/channels/channels_values_sender.py
+++ neptune/internal/channels/channels_values_sender.py
@@ -132,26 +132,21 @@ class ChannelsValuesSendingThread(NeptuneThread):
         self._sleep_time = self._SLEEP_TIME - (time.time() - send_start)
 
     def _send_values(self, queued_channels_values):
-        def channel_key(value):
-            return ChannelNameWithTypeAndNamespace(
-                value.channel_id,
-                value.channel_name,
-                value.channel_type,
-                value.channel_namespace
-            )
+        def get_channel_id(value):
+            return value.channel_id
 
-        queued_grouped_by_channel = {channel: list(values)
-                                     for channel, values
-                                     in groupby(sorted(queued_channels_values, key=channel_key),
-                                                channel_key)}
+        queued_grouped_by_channel = {channel_id: list(values)
+                                     for channel_id, values
+                                     in groupby(sorted(queued_channels_values, key=get_channel_id),
+                                                get_channel_id)}
         channels_with_values = []
-        for channel_key in queued_grouped_by_channel:
+        for channel_id in queued_grouped_by_channel:
             channel_values = []
-            for queued_value in queued_grouped_by_channel[channel_key]:
+            for queued_value in queued_grouped_by_channel[channel_id]:
                 channel_values.append(ChannelValue(ts=queued_value.channel_value.ts,
                                                    x=queued_value.channel_value.x,
                                                    y=queued_value.channel_value.y))
-            channels_with_values.append(ChannelIdWithValues(channel_key.channel_id, channel_values))
+            channels_with_values.append(ChannelIdWithValues(channel_id, channel_values))
 
         try:
             # pylint:disable=protected-access
