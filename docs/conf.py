b50986b2a4ecfc855e2899c65b32cad942381bfa
Piotr Łusakowski <piotr.lusakowski@neptune.ml>
2019-10-03 12:40:15 +0200

    Add api ref .rst files to exlude_patterns

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,16 @@ master_doc = 'index'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '404.rst']
+exclude_patterns = ['_build',
+                    'Thumbs.db',
+                    '.DS_Store',
+                    '404.rst',
+                    'experiment.rst',
+                    'neptune.rst',
+                    'notebook.rst',
+                    'project.rst',
+                    'session.rst',
+                    'utils.rst']
 
 
 # -- Options for HTML output -------------------------------------------------
