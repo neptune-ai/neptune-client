339f6be08913931e9b55ccd6fe3a30974738d113
Szymon Kuklewicz <s.kuklewicz@lsas.aero>
2019-11-04 14:39:35 +0100

    Removed unused variable

--- neptune/utils.py
+++ neptune/utils.py
@@ -201,7 +201,7 @@ def with_api_exceptions_handler(func):
                 raise SSLError()
             except (BravadoConnectionError, BravadoTimeoutError,
                     requests.exceptions.ConnectionError, requests.exceptions.Timeout,
-                    HTTPRequestTimeout, HTTPServiceUnavailable, HTTPGatewayTimeout) as ex:
+                    HTTPRequestTimeout, HTTPServiceUnavailable, HTTPGatewayTimeout):
                 if retry >= 6:
                     _logger.warning('Experiencing connection interruptions. Reestablishing communication with Neptune.')
                 time.sleep(2 ** retry)
