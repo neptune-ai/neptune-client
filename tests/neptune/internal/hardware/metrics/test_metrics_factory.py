b43adf77fb53c65b54f0d2f47a4f55f14c6fcc8b
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2020-01-15 14:41:48 +0100

    fixes

--- tests/neptune/internal/hardware/metrics/test_metrics_factory.py
+++ tests/neptune/internal/hardware/metrics/test_metrics_factory.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2020, Neptune Labs Sp. z o.o.
+# Copyright (c) 2019, Neptune Labs Sp. z o.o.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
