90c14caaae369432470f6750e211d0e33822438c
Piotr Łusakowski <piotr.lusakowskI@neptune.ai>
2020-04-01 14:11:17 +0200

    Add a condition for first iteration

--- neptune/internal/streams/channel_writer.py
+++ neptune/internal/streams/channel_writer.py
@@ -69,7 +69,7 @@ class TimeOffset(object):
         we remember the last returned value and in case of a collision, we add a microsecond.
         """
         millis_from_start = (datetime.now(tz=self._start.tzinfo) - self._start).total_seconds() * 1000
-        if self._previous_millis_from_start >= millis_from_start:
+        if self._previous_millis_from_start is not None and self._previous_millis_from_start >= millis_from_start:
             microsecond = 0.001
             self._previous_millis_from_start = self._previous_millis_from_start + microsecond
         else:
