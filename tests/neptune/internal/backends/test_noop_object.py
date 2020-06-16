8e53ae001ccd5e0c2605699e1de8df39ca042b31
Piotr Jander <pjander2@gmail.com>
2020-06-16 14:03:22 +0200

    Refactors tests to given-when-then style

--- tests/neptune/internal/backends/test_noop_object.py
+++ tests/neptune/internal/backends/test_noop_object.py
@@ -20,19 +20,40 @@ from neptune.internal.backends.offline_backend import NoopObject
 
 class TestHostedNeptuneObject(unittest.TestCase):
 
+    # given
     objectUnderTest = NoopObject()
     some_value = 42
 
     def test_builtin_fields_not_overriden(self):
-        self.assertIsInstance(self.objectUnderTest.__class__, type)
+        #when
+        result = self.objectUnderTest.__class__
+
+        # then
+        self.assertIsInstance(result, type)
 
     def test_attributes_fall_back_on_getattr(self):
-        self.assertEqual(self.objectUnderTest.foo, self.objectUnderTest)
-        self.assertEqual(self.objectUnderTest.bar(self.some_value), self.objectUnderTest)
+        #when
+        attribute = self.objectUnderTest.foo
+
+        # and
+        method_call = self.objectUnderTest.bar(self.some_value)
+
+        # then
+        self.assertEqual(attribute, self.objectUnderTest)
+
+        # and
+        self.assertEqual(method_call, self.objectUnderTest)
 
     def test_noop_object_callable(self):
-        self.assertEqual(self.objectUnderTest(self.some_value), self.objectUnderTest)
+        # when
+        result = self.objectUnderTest(self.some_value)
+
+        # then
+        self.assertEqual(result, self.objectUnderTest)
 
     def test_noop_object_context_manager(self):
+        # when
         with self.objectUnderTest as e:
+
+            # then
             e(42)
