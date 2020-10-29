8de5784ede528a6c10f48f7efca07d0604b1c282
Piotr Jander <pjander2@gmail.com>
2020-10-29 08:46:46 +0100

    Fixes tests

--- tests/neptune/alpha/test_sync.py
+++ tests/neptune/alpha/test_sync.py
@@ -57,13 +57,13 @@ def prepare_experiments(tmp_path):
     return unsync_exp, sync_exp, get_experiment_impl
 
 
-def test_list_experiments(tmp_path, monkeypatch, mocker, capsys):
+def test_list_experiments(tmp_path, mocker, capsys):
     # given
     unsync_exp, sync_exp, get_experiment_impl = prepare_experiments(tmp_path)
 
     # and
-    monkeypatch.setattr(neptune.alpha.sync, 'get_experiment', mocker.MagicMock(side_effect=get_experiment_impl))
-    monkeypatch.setattr(Operation, 'from_dict', mocker.MagicMock())
+    mocker.patch.object(neptune.alpha.sync, 'get_experiment', get_experiment_impl)
+    mocker.patch.object(Operation, 'from_dict')
 
     # when
     result = partition_experiments(tmp_path)
@@ -79,16 +79,15 @@ def test_list_experiments(tmp_path, monkeypatch, mocker, capsys):
     assert 'Synchronised experiments:\n- {}'.format(get_qualified_name(sync_exp)) in captured.out
     assert 'Unsynchronised experiments:\n- {}'.format(get_qualified_name(unsync_exp)) in captured.out
 
-def test_sync_all_experiments(tmp_path, monkeypatch, mocker, capsys):
+def test_sync_all_experiments(tmp_path, mocker, capsys):
     # given
     unsync_exp, sync_exp, get_experiment_impl = prepare_experiments(tmp_path)
 
     # and
-    execute_operations_mock = mocker.MagicMock()
-    monkeypatch.setattr(neptune.alpha.sync, 'get_experiment', mocker.MagicMock(side_effect=get_experiment_impl))
-    monkeypatch.setattr(neptune.alpha.sync, 'backend', mocker.MagicMock())
-    monkeypatch.setattr(neptune.alpha.sync.backend, 'execute_operations', execute_operations_mock)
-    monkeypatch.setattr(Operation, 'from_dict', lambda _: 'some-op')
+    mocker.patch.object(neptune.alpha.sync, 'get_experiment', get_experiment_impl)
+    mocker.patch.object(neptune.alpha.sync, 'backend')
+    mocker.patch.object(neptune.alpha.sync.backend, 'execute_operations')
+    mocker.patch.object(Operation, 'from_dict', lambda _: 'some-op')
 
     # when
     sync_all_experiments(tmp_path)
@@ -101,18 +100,18 @@ def test_sync_all_experiments(tmp_path, monkeypatch, mocker, capsys):
     assert 'Synchronising {}'.format(get_qualified_name(sync_exp)) not in captured.out
 
     # and
-    execute_operations_mock.assert_called_once_with(unsync_exp.uuid, ['some-op'])
+    # pylint: disable=no-member
+    neptune.alpha.sync.backend.execute_operations.assert_called_once_with(unsync_exp.uuid, ['some-op'])
 
-def test_sync_selected_experiments(tmp_path, monkeypatch, mocker, capsys):
+def test_sync_selected_experiments(tmp_path, mocker, capsys):
     # given
     unsync_exp, sync_exp, get_experiment_impl = prepare_experiments(tmp_path)
 
     # and
-    execute_operations_mock = mocker.MagicMock()
-    monkeypatch.setattr(neptune.alpha.sync, 'get_experiment', mocker.MagicMock(side_effect=get_experiment_impl))
-    monkeypatch.setattr(neptune.alpha.sync, 'backend', mocker.MagicMock())
-    monkeypatch.setattr(neptune.alpha.sync.backend, 'execute_operations', execute_operations_mock)
-    monkeypatch.setattr(Operation, 'from_dict', lambda _: 'some-op')
+    mocker.patch.object(neptune.alpha.sync, 'get_experiment', get_experiment_impl)
+    mocker.patch.object(neptune.alpha.sync, 'backend')
+    mocker.patch.object(neptune.alpha.sync.backend, 'execute_operations')
+    mocker.patch.object(Operation, 'from_dict', lambda _: 'some-op')
 
     # when
     sync_selected_experiments(tmp_path, [get_qualified_name(sync_exp)])
@@ -125,4 +124,5 @@ def test_sync_selected_experiments(tmp_path, monkeypatch, mocker, capsys):
     assert 'Synchronising {}'.format(get_qualified_name(unsync_exp)) not in captured.out
 
     # and
-    execute_operations_mock.assert_not_called()
+    # pylint: disable=no-member
+    neptune.alpha.sync.backend.execute_operations.assert_not_called()
