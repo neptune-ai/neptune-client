26389b230f20c25705dd84bfbb195c759cee05a0
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2021-02-01 10:38:46 +0100

    Add copyright to integration kit

--- alpha_integration_dev/common_client_code.py
+++ alpha_integration_dev/common_client_code.py
@@ -1,9 +1,21 @@
-from datetime import datetime
-import sys
-import abc
+#
+# Copyright (c) 2021, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-import neptune
-from neptune import alpha as alpha_neptune
+from datetime import datetime
+import abc
 
 
 class ClientFeatures(abc.ABC):
