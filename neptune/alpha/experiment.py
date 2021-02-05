ac89632cf3fad4d9259fcdb4b4a4435b38de497e
Adam Nieżurawski <adam.niezurawski@neptune.ml>
2021-02-05 12:36:38 +0100

    Pretty print of experiment's structure (#395)

--- neptune/alpha/experiment.py
+++ neptune/alpha/experiment.py
@@ -23,6 +23,8 @@ from datetime import datetime
 from io import IOBase
 from typing import Dict, Any, Union, List, Optional
 
+import click
+
 from neptune.alpha.attributes.atoms.datetime import Datetime as DatetimeAttr
 from neptune.alpha.attributes.atoms.experiment_state import ExperimentState as ExperimentStateAttr
 from neptune.alpha.attributes.atoms.file import File as FileAttr
@@ -52,6 +54,7 @@ from neptune.alpha.types.atoms.float import Float
 from neptune.alpha.types.atoms.string import String
 from neptune.alpha.types.atoms.file import File
 from neptune.alpha.types.value import Value
+from neptune.exceptions import UNIX_STYLES
 
 
 class Experiment(AbstractContextManager):
@@ -110,6 +113,25 @@ class Experiment(AbstractContextManager):
 
     def get_structure(self) -> Dict[str, Any]:
         return self._structure.get_structure()
+
+    def print_structure(self) -> None:
+        self._print_structure_impl(self.get_structure(), indent=0)
+
+    def _print_structure_impl(self, struct: dict, indent: int) -> None:
+        for key in sorted(struct.keys()):
+            click.echo("    " * indent, nl=False)
+            if isinstance(struct[key], dict):
+                click.echo("{blue}'{key}'{end}:".format(
+                    blue=UNIX_STYLES['blue'],
+                    key=key,
+                    end=UNIX_STYLES['end']))
+                self._print_structure_impl(struct[key], indent=indent+1)
+            else:
+                click.echo("{blue}'{key}'{end}: {type}".format(
+                    blue=UNIX_STYLES['blue'],
+                    key=key,
+                    end=UNIX_STYLES['end'],
+                    type=type(struct[key]).__name__))
 
     def define(self,
                path: str,
