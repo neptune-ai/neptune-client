88fc30df3f67cb79fc7d57c200bff4a61d707bd3
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-03-19 12:26:40 +0100

    added imports

--- integrations/sklearn/neptune_sklearn/__init__.py
+++ integrations/sklearn/neptune_sklearn/__init__.py
@@ -28,8 +28,8 @@ from yellowbrick.model_selection import FeatureImportances
 from yellowbrick.regressor import ResidualsPlot, PredictionError, CooksDistance
 
 import neptune.new as neptune
-
 from ._version import get_versions
+
 __version__ = get_versions()['version']
 del get_versions
 
