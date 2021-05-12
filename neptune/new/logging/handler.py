2fdb737077c56083b0b56c5419aa5b932e67ee78
Piotr Kasprzyk <piotr.kasprzyk@neptune.ai>
2021-05-12 13:40:13 +0200

    basic docstring for NeptuneLogHandler

--- neptune/new/logging/handler.py
+++ neptune/new/logging/handler.py
@@ -22,6 +22,31 @@ from neptune.new.logging import Logger
 
 
 class NeptuneLogHandler(logging.Handler):
+    """Custom handler that sends logged messages to Neptune
+
+    Args:
+        run (Run): An existing run reference (as returned by `neptune.init`)
+            Logger will send messages as a `StringSeries` attribute on this run.
+        level (int, optional): Log level of the handler. Defaults to `logging.NOTSET`,
+            which logs everything that matches logger's level.
+        path (str, optional): Path to the `StringSeries` attribute used for logging. Default to `None`.
+            If `None`, `'monitoring/python_logger'` is used.
+
+    Usage example:
+        >>> import logging
+        >>> import neptune.new as neptune
+        >>> from neptune.new.logging.handler import NeptuneLogHandler
+
+        >>> logger = logging.getLogger("root_experiment")
+        >>> logger.setLevel(logging.DEBUG)
+
+        >>> run = neptune.init(project=‘neptune/sandbox’)
+        >>> npt_handler = NeptuneLoggerHandler(run=run, level=logging.WARNING, path="logging/root_experiment")
+        >>> logger.addHandler(npt_handler)
+
+        >>> logger.debug("this won't be sent")
+        >>> logger.error("but this will!!%d", 1)
+    """
     def __init__(self, *, run: Run, level=logging.NOTSET, path: str = None):
         verify_type("run", run, Run)
         verify_type("level", level, int)
