06bc7855038c366a7c749f6a94b33b89ff510054
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2021-05-17 13:07:02 +0200

    Add neptune-pytorch-lightning-integration

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ def main():
             cmdclass=versioneer.get_cmdclass(),
             extras_require={
               "lightgbm": ["neptune-lightgbm"],
+              "pytorch-lightning": ["neptune-pytorch-lightning"],
               "sacred": ["neptune-sacred"],
               "sklearn": ["neptune-sklearn"],
               "tensorflow-keras": ["neptune-tensorflow-keras"],
