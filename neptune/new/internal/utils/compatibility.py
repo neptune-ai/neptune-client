64b5eb04b0b1ae8689934c449eb25bdddd4c811b
Piotr Kasprzyk <piotr.kasprzyk@neptune.ai>
2021-07-06 11:07:54 +0200

    make linter happy

--- neptune/new/internal/utils/compatibility.py
+++ neptune/new/internal/utils/compatibility.py
@@ -19,6 +19,6 @@ from neptune.new import Run
 from neptune.new.exceptions import NeptuneLegacyIncompatibilityException
 
 
-def expect_not_an_experiment(run: 'Run'):
+def expect_not_an_experiment(run: Run):
     if isinstance(run, Experiment):
         raise NeptuneLegacyIncompatibilityException()
