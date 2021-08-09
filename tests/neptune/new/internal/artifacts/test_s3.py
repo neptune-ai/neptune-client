21bfcae118a45ef4b715ca32864119ab753408db
Rafal Jankowski <rafal.jankowski@neptune.ai>
2021-08-09 21:41:18 +0200

    Test fix

--- tests/neptune/new/internal/artifacts/test_s3.py
+++ tests/neptune/new/internal/artifacts/test_s3.py
@@ -74,7 +74,7 @@ class TestS3ArtifactDriversMap(unittest.TestCase):
 
         self.assertEqual(1, len(files))
         self.assertIsInstance(files[0], ArtifactFileData)
-        self.assertEqual(ArtifactFileType.S3, files[0].type)
+        self.assertEqual(ArtifactFileType.S3.value, files[0].type)
         self.assertEqual('2f249230a8e7c2bf6005ccd2679259ec', files[0].file_hash)
         self.assertEqual('.', files[0].file_path)
         self.assertEqual(
