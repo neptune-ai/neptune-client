7e22ee16167580d80dbcca4e05b7cac1c9993d46
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2021-08-16 19:12:37 +0200

    Fix get_tracked_files

--- neptune/new/internal/artifacts/drivers/local.py
+++ neptune/new/internal/artifacts/drivers/local.py
@@ -51,7 +51,8 @@ class LocalArtifactDriver(ArtifactDriver):
 
     @classmethod
     def get_tracked_files(cls, path: str, namespace: str = None) -> typing.List[ArtifactFileData]:
-        parsed_path = urlparse(path).path
+        path_parts = urlparse(path)
+        parsed_path = f'{path_parts.netloc}{path_parts.path}'
         source_location = pathlib.Path(parsed_path)
 
         stored_files: typing.List[ArtifactFileData] = list()
