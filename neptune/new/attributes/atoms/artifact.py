9bb3bd2514beef8c2b1730ded713aa42b45e0d0a
Piotr Kasprzyk <piotr.kasprzyk@neptune.ai>
2021-08-24 15:07:54 +0200

    add Artifact.fetch() and make Artifact.assign() public

--- neptune/new/attributes/atoms/artifact.py
+++ neptune/new/attributes/atoms/artifact.py
@@ -24,13 +24,16 @@ from neptune.new.types.atoms.artifact import Artifact as ArtifactVal
 
 
 class Artifact(Atom):
-    def _assign(self, value: typing.Union[ArtifactVal, str], wait: bool = False):
+    def assign(self, value: typing.Union[ArtifactVal, str], wait: bool = False):
         # this function should not be publicly available
         if not isinstance(value, ArtifactVal):
             value = ArtifactVal(value)
 
         with self._run.lock():
             self._enqueue_operation(AssignArtifact(self._path, value.hash), wait)
+
+    def fetch(self) -> ArtifactVal:
+        return ArtifactVal(self.fetch_hash())
 
     def fetch_hash(self) -> str:
         val = self._backend.get_artifact_attribute(self._run_uuid, self._path)
