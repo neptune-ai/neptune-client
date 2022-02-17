ec232f7882b5b7ee11031fc7a39e330669ca24bc
Piotr Kasprzyk <piotr.kasprzyk@neptune.ai>
2022-02-17 16:36:59 +0100

    linter

--- e2e_tests/standard/test_init.py
+++ e2e_tests/standard/test_init.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 #
 import pytest
-from neptune.new.metadata_containers import MetadataContainer
 
 import neptune.new as neptune
 from neptune.new.project import Project
