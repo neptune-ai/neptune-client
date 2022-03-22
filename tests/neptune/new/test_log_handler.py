acd5ddb70d96648cd7b80cc0086253cca13560fe
Piotr Kasprzyk <piotr.kasprzyk@neptune.ai>
2022-03-22 14:07:18 +0100

    Revert "WIP fail test"
    This reverts commit d378c8cee0c1abcd72a2e22691b6dbf49ae10ec4.

--- tests/neptune/new/test_log_handler.py
+++ tests/neptune/new/test_log_handler.py
@@ -15,7 +15,6 @@
 #
 import logging
 import os
-import sys
 import unittest
 
 from neptune.new import ANONYMOUS, init
@@ -35,7 +34,6 @@ class TestLogHandler(unittest.TestCase):
         logger.warning("test %s", "message")
 
     def test_no_run(self):
-        assert sys.version_info.minor > 6
         with self.assertRaises(TypeError):
             NeptuneHandler(run="PET-1")
 
