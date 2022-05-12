86102d23fc97540732cae6c1505600973c5549ed
wajcha <aniezurawski@gmail.com>
2022-05-12 09:10:29 +0200

    Fix

--- neptune/new/internal/init/model.py
+++ neptune/new/internal/init/model.py
@@ -65,18 +65,6 @@ def init_model(
     # make mode proper Enum instead of string
     mode = Mode(mode)
 
-    # TODO: Do not duplicate validation logic from server.
-    # Instead, design server-side Validation Errors that can be pretty printed in client.
-    if key:
-        if len(key) > 10:
-            raise ValueError("Model key cannot be longer than 10 characters")
-        if key != key.upper():
-            raise ValueError("Model key cannot contain lowercase letters")
-        if not re.match(r"^[A-Z0-9]{1,10}$", key):
-            raise ValueError("Model key should contain only capital letters and digits")
-        if re.match(r"^[0-9]*$", key):
-            raise ValueError("Model key cannot consist of digits only")
-
     if mode == Mode.OFFLINE:
         raise NeptuneException("Model can't be initialized in OFFLINE mode")
 
