dfb600d7fa217d60bd9722b7ff0b3ddb28e00e55
Rafał Jankowski <rafal.jankowski@neptune.ai>
2022-05-31 11:31:04 +0200

    Update neptune/new/internal/backends/utils.py
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>

--- neptune/new/internal/backends/utils.py
+++ neptune/new/internal/backends/utils.py
@@ -198,7 +198,7 @@ def verify_client_version(client_config: ClientConfig, version: Version):
         and client_config.version_info.min_recommended > version
     ):
         click.echo(
-            f"WARNING: Your version of Neptune client library ({version}) is deprecated,"
+            f"WARNING: Your version of the Neptune client library ({version}) is deprecated,"
             " and soon will no longer be supported by the Neptune server."
             f" We recommend an upgrade to at least version {client_config.version_info.min_recommended}.",
             sys.stderr,
