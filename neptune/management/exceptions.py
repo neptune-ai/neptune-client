a27d09e28dc8de366613970f6dffa3d171cecce4
Rafał Jankowski <rafal.jankowski@neptune.ai>
2022-07-27 20:10:32 +0200

    Full name of service account instead of short name (#955)

--- neptune/management/exceptions.py
+++ neptune/management/exceptions.py
@@ -127,6 +127,13 @@ class ServiceAccountNotExistsOrWithoutAccess(ManagementOperationFailure):
     )
 
 
+class ServiceAccountNotFound(ManagementOperationFailure):
+    code = 16
+    description = (
+        'Service account "{service_account_name}" could not be found in workspace "{workspace}"'
+    )
+
+
 class BadRequestException(ManagementOperationFailure):
     code = 400
     description = (
