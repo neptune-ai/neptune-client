a46834893d5931f813ebf9c5e7b7c3e2c1a96a07
Rafał Jankowski <rafal.jankowski@neptune.ai>
2022-08-03 14:55:24 +0200

    Initial HuggingFace E2E tests - proper run lifecycle (#945)

--- e2e_tests/utils.py
+++ e2e_tests/utils.py
@@ -30,6 +30,7 @@ import string
 import tempfile
 from contextlib import contextmanager
 from datetime import datetime
+from time import perf_counter
 
 import numpy
 from attr import dataclass
@@ -176,3 +177,44 @@ def reinitialize_container(sys_id: str, container_type: str, project: str):
         return neptune.init_model_version(version=sys_id, project=project)
 
     raise NotImplementedError()
+
+
+# from https://stackoverflow.com/a/62956469
+@contextmanager
+def catch_time() -> float:
+    start = perf_counter()
+    yield lambda: perf_counter() - start
+
+
+# from https://stackoverflow.com/a/34333710
+@contextmanager
+def modified_environ(*remove, **update):
+    """
+    Temporarily updates the ``os.environ`` dictionary in-place.
+
+    The ``os.environ`` dictionary is updated in-place so that the modification
+    is sure to work in all situations.
+
+    :param remove: Environment variables to remove.
+    :param update: Dictionary of environment variables and values to add/update.
+    """
+    env = os.environ
+    update = update or {}
+    remove = remove or []
+
+    # List of environment variables being updated or removed.
+    stomped = (set(update.keys()) | set(remove)) & set(env.keys())
+    # Environment variables and values to restore on exit.
+    update_after = {k: env[k] for k in stomped}
+    # Environment variables and values to remove on exit.
+    remove_after = frozenset(k for k in update if k not in env)
+
+    try:
+        env.update(update)
+        for k in remove:
+            env.pop(k, None)
+        yield
+    finally:
+        env.update(update_after)
+        for k in remove_after:
+            env.pop(k)
