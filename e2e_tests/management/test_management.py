f6b86780ddeaa660fcef722150744e95506c7a02
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2022-08-05 22:53:43 +0200

    test_invite_as_non_admin

--- e2e_tests/management/test_management.py
+++ e2e_tests/management/test_management.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from typing import Dict
+
 import pytest
 
 from e2e_tests.base import BaseE2ETest, fake
@@ -178,7 +180,7 @@ class TestManagement(BaseE2ETest):
             api_token=environment.admin_token,
         )
 
-        assert created_project_identifier in get_project_list(api_token=environment.user_token)
+        assert created_project_identifier in get_project_list(api_token=environment.admin_token)
 
         with pytest.raises(UserNotExistsOrWithoutAccess):
             remove_project_member(
@@ -198,13 +200,12 @@ class TestManagement(BaseE2ETest):
         assert project_identifier not in get_project_list(api_token=environment.admin_token)
 
     def test_create_project(self, environment: "Environment"):
-        project_name, project_key = a_project_name(project_slug=f"{fake.slug()}-workspace")
+        project_name, project_key = a_project_name(project_slug=f"{fake.slug()}-creat")
         project_identifier = normalize_project_name(
             name=project_name, workspace=environment.workspace
         )
 
         assert project_identifier not in get_project_list(api_token=environment.user_token)
-
         assert environment.user in get_workspace_member_list(
             name=environment.workspace, api_token=environment.user_token
         )
@@ -218,13 +219,125 @@ class TestManagement(BaseE2ETest):
         created_project_identifier = create_project(
             name=project_name,
             key=project_key,
-            visibility="workspace",
             workspace=environment.workspace,
             api_token=environment.user_token,
         )
 
         assert created_project_identifier == project_identifier
         assert created_project_identifier in get_project_list(api_token=environment.user_token)
+
+        delete_project(name=created_project_identifier, api_token=environment.admin_token)
+
+        assert project_identifier not in get_project_list(api_token=environment.user_token)
+
+    def _assure_presence_and_role(
+        self, *, username: str, expected_role: str, member_list: Dict[str, str]
+    ):
+        assert username in member_list
+        assert member_list.get(username) == expected_role
+
+    def _test_add_sa_to_project_as_owner(
+        self, created_project_identifier: str, environment: "Environment"
+    ):
+        self._assure_presence_and_role(
+            username=environment.user,
+            expected_role="owner",
+            member_list=get_project_member_list(
+                name=created_project_identifier, api_token=environment.user_token
+            ),
+        )
+
+        assert environment.service_account not in get_project_service_account_list(
+            name=created_project_identifier, api_token=environment.user_token
+        )
+
+        add_project_service_account(
+            name=created_project_identifier,
+            service_account_name=environment.service_account,
+            role="contributor",
+            api_token=environment.admin_token,
+        )
+        self._assure_presence_and_role(
+            username=environment.service_account,
+            expected_role="contributor",
+            member_list=get_project_service_account_list(
+                name=created_project_identifier, api_token=environment.user_token
+            ),
+        )
+
+        remove_project_service_account(
+            name=created_project_identifier,
+            service_account_name=environment.service_account,
+            api_token=environment.admin_token,
+        )
+        assert environment.service_account not in get_project_service_account_list(
+            name=created_project_identifier, api_token=environment.admin_token
+        )
+
+    def _test_add_user_to_project_as_sa(
+        self, created_project_identifier: str, environment: "Environment"
+    ):
+        self._assure_presence_and_role(
+            username=environment.service_account,
+            expected_role="owner",
+            member_list=get_project_service_account_list(
+                name=created_project_identifier, api_token=environment.user_token
+            ),
+        )
+
+        assert environment.user not in get_project_member_list(
+            name=created_project_identifier, api_token=environment.user_token
+        )
+
+        add_project_member(
+            name=created_project_identifier,
+            username=environment.user,
+            role="contributor",
+            api_token=environment.admin_token,
+        )
+        self._assure_presence_and_role(
+            username=environment.user,
+            expected_role="contributor",
+            member_list=get_project_member_list(
+                name=created_project_identifier, api_token=environment.user_token
+            ),
+        )
+
+        remove_project_member(
+            name=created_project_identifier,
+            username=environment.user,
+            api_token=environment.admin_token,
+        )
+        assert environment.user not in get_project_member_list(
+            name=created_project_identifier, api_token=environment.admin_token
+        )
+
+    def test_invite_as_non_admin(self, environment: "Environment"):
+        project_name, project_key = a_project_name(project_slug=f"{fake.slug()}-invit")
+        project_identifier = normalize_project_name(
+            name=project_name, workspace=environment.workspace
+        )
+
+        created_project_identifier = create_project(
+            name=project_name,
+            key=project_key,
+            workspace=environment.workspace,
+            api_token=environment.user_token,
+        )
+
+        assert created_project_identifier == project_identifier
+        assert created_project_identifier in get_project_list(api_token=environment.user_token)
+
+        # user who created a project (`user_token`) will be automatically project owner
+        sa_is_project_owner = environment.service_account in get_project_service_account_list(
+            name=created_project_identifier, api_token=environment.user_token
+        )
+        if sa_is_project_owner:
+            # SA has access to project, so tests are run as SA
+            self._test_add_user_to_project_as_sa(created_project_identifier, environment)
+        else:
+            # SA doesn't have access to project, so tests are run as user
+            self._test_add_sa_to_project_as_owner(created_project_identifier, environment)
 
         delete_project(name=created_project_identifier, api_token=environment.admin_token)
 
