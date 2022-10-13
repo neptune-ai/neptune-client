7014339718dceacd25490547adf6db0d360d442d
Rafal Jankowski <rafal.jankowski@neptune.ai>
2022-10-13 14:29:21 +0200

    Imports

--- src/neptune/checkpoint.py
+++ src/neptune/checkpoint.py
@@ -13,28 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from neptune.legacy.utils import (
-    IS_MACOS,
-    IS_WINDOWS,
-    NoopObject,
-    align_channels_on_x,
-    as_list,
-    assure_directory_exists,
-    assure_project_qualified_name,
-    discover_git_repo_location,
-    file_contains,
-    get_channel_name_stems,
-    get_git_info,
-    glob,
-    in_docker,
-    is_float,
-    is_ipython,
-    is_nan_or_inf,
-    is_notebook,
-    map_keys,
-    map_values,
-    merge_dataframes,
-    update_session_proxies,
-    validate_notebook_path,
-    with_api_exceptions_handler,
-)
+from neptune.legacy.checkpoint import Checkpoint
