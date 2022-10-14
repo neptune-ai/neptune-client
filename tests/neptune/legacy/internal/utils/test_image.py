04e509afaa5ead0d1a90157ea280b55f4752ce13
Rafal Jankowski <rafal.jankowski@neptune.ai>
2022-10-14 12:33:21 +0200

    Ignored pylint in test_image

--- tests/neptune/legacy/internal/utils/test_image.py
+++ tests/neptune/legacy/internal/utils/test_image.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# pylint: disable=wrong-import-order,wrong-import-position,unused-variable,no-member,no-value-for-parameter
 import os
 import sys
 import unittest
