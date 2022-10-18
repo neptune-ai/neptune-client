ef5852c6239871bf26de23bff73865a6e20bdc09
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2022-10-18 09:32:06 +0200

    Add __all__ to operation_storage.py

--- src/neptune/new/internal/operation_processors/operation_storage.py
+++ src/neptune/new/internal/operation_processors/operation_storage.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+__all__ = ("OperationStorage",)
+
 import abc
 
 from neptune.new.internal.container_type import ContainerType
