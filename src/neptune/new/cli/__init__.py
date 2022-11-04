118a333a9dda6b5ddfa3afd9b3821f7d6ad192f5
PatrykGala <fotpatryk@gmail.com>
2022-11-04 14:59:17 +0100

    move sync command to new package (#1078)
    * Move CLI to new package: neptune.new.cli
    
    * Move CLI to new package: neptune.new.cli
    
    * Move sync test to new package: neptune.new.cli
    
    * Move sync e2e tests to new package: neptune.new.cli
    
    * Add backward compatibility for imports
    
    * PR changes
    
    * Update changelog
    
    Co-authored-by: Patryk Gala <patryk.gala@neptune.ai>

--- src/neptune/new/cli/__init__.py
+++ src/neptune/new/cli/__init__.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019, Neptune Labs Sp. z o.o.
+# Copyright (c) 2022, Neptune Labs Sp. z o.o.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,24 +14,9 @@
 # limitations under the License.
 #
 
-import click
-import pkg_resources
+__all__ = ["sync", "status"]
 
-from neptune.new.cli import (
+from neptune.new.cli.commands import (
     status,
     sync,
 )
-
-
-@click.group()
-def main():
-    pass
-
-
-main.add_command(sync)
-main.add_command(status)
-
-plugins = {entry_point.name: entry_point for entry_point in pkg_resources.iter_entry_points("neptune.plugins")}
-
-for name, entry_point in plugins.items():
-    main.add_command(entry_point.load(), name)
