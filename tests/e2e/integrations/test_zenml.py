bfff3d8840350ae6b871ef7220fff858f14006c3
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2022-12-08 12:10:02 +0100

    fix neptune run fetching test

--- tests/e2e/integrations/test_zenml.py
+++ tests/e2e/integrations/test_zenml.py
@@ -123,14 +123,16 @@ class TestZenML(BaseE2ETest):
 
         self.zenml_run_name = run.get_runs()[-1].name
 
-    def _test_fetch_neptune_run(self):
+    def _test_fetch_neptune_run(self, environment):
         custom_run_id = hashlib.md5(self.zenml_run_name.encode()).hexdigest()
-        neptune_run = neptune.init_run(custom_run_id=custom_run_id)
+        neptune_run = neptune.init_run(
+            custom_run_id=custom_run_id, project=environment.project, api_token=environment.user_token
+        )
         assert neptune_run["params/gamma"].fetch() == 0.001
         assert neptune_run["sys/tags"].fetch() == {"sklearn", "digits"}
         assert neptune_run["metrics/val_accuracy"].fetch() <= 1
 
-    def test_zenml(self, registered_stack, zenml_client):
+    def test_zenml(self, registered_stack, zenml_client, environment):
         self._test_setup_creates_stack_with_neptune_experiment_tracker(zenml_client)
         self._test_pipeline_runs_without_errors()
-        self._test_fetch_neptune_run()
+        self._test_fetch_neptune_run(environment)
