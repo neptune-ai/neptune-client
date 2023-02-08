fad56300f30aede431dbbe546a254cd7e3853165
Rafal Jankowski <rafal.jankowski@neptune.ai>
2023-02-08 15:33:18 +0100

    Test deduplication

--- tests/e2e/standard/test_series.py
+++ tests/e2e/standard/test_series.py
@@ -14,7 +14,10 @@
 # limitations under the License.
 #
 import random
-from typing import List
+from typing import (
+    Any,
+    List,
+)
 
 import pytest
 from PIL import Image
@@ -32,27 +35,13 @@ from tests.e2e.utils import (
     tmp_context,
 )
 
+SERIES_VALUES = ["strings", "floats"]
+
 
 class TestSeries(BaseE2ETest):
+    @pytest.mark.parametrize("values", SERIES_VALUES, indirect=True)
     @pytest.mark.parametrize("container", AVAILABLE_CONTAINERS, indirect=True)
-    def test_log_numbers(self, container: MetadataContainer, key: str):
-        # given
-        values = list(random.random() for _ in range(50))
-
-        # when
-        container[key].log(values[0])
-        container[key].log(values[1:])
-        container.sync()
-
-        # then
-        assert container[key].fetch_last() == values[-1]
-        assert list(container[key].fetch_values()["value"]) == values
-
-    @pytest.mark.parametrize("container", AVAILABLE_CONTAINERS, indirect=True)
-    def test_log_strings(self, container: MetadataContainer, key: str):
-        # given
-        values = list(fake.word() for _ in range(50))
-
+    def test_log(self, container: MetadataContainer, values: List[Any], key: str):
         # when
         container[key].log(values[0])
         container[key].log(values[1:])
@@ -75,25 +64,9 @@ class TestSeries(BaseE2ETest):
         # then
         assert_images(container=container, key=key, images=images)
 
+    @pytest.mark.parametrize("values", SERIES_VALUES, indirect=True)
     @pytest.mark.parametrize("container", AVAILABLE_CONTAINERS, indirect=True)
-    def test_append_numbers(self, container: MetadataContainer, key: str):
-        # given
-        values = list(random.random() for _ in range(50))
-
-        # when
-        for value in values:
-            container[key].append(value)
-        container.sync()
-
-        # then
-        assert container[key].fetch_last() == values[-1]
-        assert list(container[key].fetch_values()["value"]) == values
-
-    @pytest.mark.parametrize("container", AVAILABLE_CONTAINERS, indirect=True)
-    def test_append_strings(self, container: MetadataContainer, key: str):
-        # given
-        values = list(fake.word() for _ in range(50))
-
+    def test_append(self, container: MetadataContainer, values: List[Any], key: str):
         # when
         for value in values:
             container[key].append(value)
@@ -117,25 +90,9 @@ class TestSeries(BaseE2ETest):
         # then
         assert_images(container=container, key=key, images=images)
 
+    @pytest.mark.parametrize("values", SERIES_VALUES, indirect=True)
     @pytest.mark.parametrize("container", AVAILABLE_CONTAINERS, indirect=True)
-    def test_extend_numbers(self, container: MetadataContainer, key: str):
-        # given
-        values = list(random.random() for _ in range(50))
-
-        # when
-        container[key].extend([values[0]])
-        container[key].extend(values[1:])
-        container.sync()
-
-        # then
-        assert container[key].fetch_last() == values[-1]
-        assert list(container[key].fetch_values()["value"]) == values
-
-    @pytest.mark.parametrize("container", AVAILABLE_CONTAINERS, indirect=True)
-    def test_extend_strings(self, container: MetadataContainer, key: str):
-        # given
-        values = list(fake.word() for _ in range(50))
-
+    def test_extend(self, container: MetadataContainer, values: List[Any], key: str):
         # when
         container[key].extend([values[0]])
         container[key].extend(values[1:])
@@ -164,7 +121,20 @@ class TestSeries(BaseE2ETest):
         yield self.gen_key()
 
 
-def assert_images(container: MetadataContainer, key: str, images: List[Image]):
+@pytest.fixture()
+def values(request):
+    if request.param == "floats":
+        yield list(random.random() for _ in range(50))
+
+    elif request.param == "strings":
+        yield list(fake.word() for _ in range(50))
+
+    elif request.param == "images":
+        # images with size between 200KB - 12MB
+        yield list(generate_image(size=2**n) for n in range(8, 12))
+
+
+def assert_images(container: MetadataContainer, key: str, images: List["Image"]):
     with tmp_context():
         container[key].download_last("last")
         container[key].download("all")
