c36d58dc1c3f6714fc756a3a3048822a86830c83
Rafal Jankowski <rafal.jankowski@neptune.ai>
2023-02-10 14:34:07 +0100

    Moved init

--- src/neptune/__init__.py
+++ src/neptune/__init__.py
@@ -13,10 +13,122 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-__all__ = ["__version__"]
+__all__ = [
+    "types",
+    "ANONYMOUS",
+    "ANONYMOUS_API_TOKEN",
+    "NeptunePossibleLegacyUsageException",
+    "NeptuneUninitializedException",
+    "get_project",
+    "init",
+    "init_model",
+    "init_model_version",
+    "init_project",
+    "init_run",
+    "Run",
+    "__version__",
+    "create_experiment",
+    "get_experiment",
+    "append_tag",
+    "append_tags",
+    "remove_tag",
+    "set_property",
+    "remove_property",
+    "send_metric",
+    "log_metric",
+    "send_text",
+    "log_text",
+    "send_image",
+    "log_image",
+    "send_artifact",
+    "delete_artifacts",
+    "log_artifact",
+    "stop",
+    "get_last_run",
+]
+
+from typing import Optional
 
 from neptune.common.patches import apply_patches
-from neptune.version import __version__
+from neptune.new import types
+from neptune.new.constants import (
+    ANONYMOUS,
+    ANONYMOUS_API_TOKEN,
+)
+from neptune.new.exceptions import (
+    NeptunePossibleLegacyUsageException,
+    NeptuneUninitializedException,
+)
+from neptune.new.internal.init import (
+    get_project,
+    init,
+    init_model,
+    init_model_version,
+    init_project,
+    init_run,
+)
+from neptune.new.internal.utils.deprecation import deprecated
+from neptune.new.metadata_containers import Run
+from neptune.version import version
+
+
+def _raise_legacy_client_expected(*args, **kwargs):
+    raise NeptunePossibleLegacyUsageException()
+
+
+create_experiment = (
+    get_experiment
+) = (
+    append_tag
+) = (
+    append_tags
+) = (
+    remove_tag
+) = (
+    set_property
+) = (
+    remove_property
+) = (
+    send_metric
+) = (
+    log_metric
+) = (
+    send_text
+) = (
+    log_text
+) = send_image = log_image = send_artifact = delete_artifacts = log_artifact = stop = _raise_legacy_client_expected
+
+
+@deprecated()
+def get_last_run() -> Optional[Run]:
+    """Returns last created Run object.
+
+    Returns:
+        ``Run``: object last created by neptune global object.
+
+    Examples:
+        >>> import neptune.new as neptune
+
+        >>> # Crate a new tracked run
+        ... neptune.init_run(name='A new approach', source_files='**/*.py')
+        ... # Oops! We didn't capture the reference to the Run object
+
+        >>> # Not a problem! We've got you covered.
+        ... run = neptune.get_last_run()
+
+    You may also want to check `get_last_run docs page`_.
+
+    .. _get_last_run docs page:
+       https://docs.neptune.ai/api/neptune/#get_last_run
+    """
+    last_run = Run.last_run
+    if last_run is None:
+        raise NeptuneUninitializedException()
+    return last_run
+
+
+__version__ = str(version)
+
 
 # Apply patches of external libraries
 apply_patches()
--- src/neptune/new/__init__.py
+++ src/neptune/new/__init__.py
@@ -57,82 +57,36 @@ __all__ = [
     "get_last_run",
 ]
 
-from typing import Optional
-
-from neptune.new import types
-from neptune.new.constants import (
+from neptune import (
     ANONYMOUS,
     ANONYMOUS_API_TOKEN,
-)
-from neptune.new.exceptions import (
     NeptunePossibleLegacyUsageException,
     NeptuneUninitializedException,
-)
-from neptune.new.internal.init import (
+    Run,
+    __version__,
+    append_tag,
+    append_tags,
+    create_experiment,
+    delete_artifacts,
+    get_experiment,
+    get_last_run,
     get_project,
     init,
     init_model,
     init_model_version,
     init_project,
     init_run,
+    log_artifact,
+    log_image,
+    log_metric,
+    log_text,
+    remove_property,
+    remove_tag,
+    send_artifact,
+    send_image,
+    send_metric,
+    send_text,
+    set_property,
+    stop,
+    types,
 )
-from neptune.new.internal.utils.deprecation import deprecated
-from neptune.new.metadata_containers import Run
-from neptune.new.version import version
-
-__version__ = str(version)
-
-
-def _raise_legacy_client_expected(*args, **kwargs):
-    raise NeptunePossibleLegacyUsageException()
-
-
-create_experiment = (
-    get_experiment
-) = (
-    append_tag
-) = (
-    append_tags
-) = (
-    remove_tag
-) = (
-    set_property
-) = (
-    remove_property
-) = (
-    send_metric
-) = (
-    log_metric
-) = (
-    send_text
-) = (
-    log_text
-) = send_image = log_image = send_artifact = delete_artifacts = log_artifact = stop = _raise_legacy_client_expected
-
-
-@deprecated()
-def get_last_run() -> Optional[Run]:
-    """Returns last created Run object.
-
-    Returns:
-        ``Run``: object last created by neptune global object.
-
-    Examples:
-        >>> import neptune.new as neptune
-
-        >>> # Crate a new tracked run
-        ... neptune.init_run(name='A new approach', source_files='**/*.py')
-        ... # Oops! We didn't capture the reference to the Run object
-
-        >>> # Not a problem! We've got you covered.
-        ... run = neptune.get_last_run()
-
-    You may also want to check `get_last_run docs page`_.
-
-    .. _get_last_run docs page:
-       https://docs.neptune.ai/api/neptune/#get_last_run
-    """
-    last_run = Run.last_run
-    if last_run is None:
-        raise NeptuneUninitializedException()
-    return last_run
