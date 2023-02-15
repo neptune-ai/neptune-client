51677341a17418b9cc15289b2a83b99eb0dc0850
Rafal Jankowski <rafal.jankowski@neptune.ai>
2023-02-15 08:55:00 +0100

    Tests added

--- src/neptune/internal/utils/hashing.py
+++ src/neptune/internal/utils/hashing.py
@@ -1,3 +1,18 @@
+#
+# Copyright (c) 2023, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 __all__ = ["generate_hash"]
 
 import hashlib
@@ -5,4 +20,5 @@ import hashlib
 
 def generate_hash(*descriptors, max_length: int = 8):
     combined = "_".join(map(str, descriptors))
-    return hashlib.sha256(combined.encode(), usedforsecurity=False).hexdigest()[-max_length:]
+    hexadecimal = hashlib.sha256(combined.encode(), usedforsecurity=False).hexdigest()
+    return hexadecimal[-max_length:]
