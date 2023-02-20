c22e67d40c682a2476249d1363a5ffc1d4145068
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-02-20 22:15:48 +0100

    add unit tests

---
+++ tests/unit/neptune/new/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+from neptune import init_project
+from neptune.metadata_containers import Project
+
+
+@pytest.fixture(scope="session")
+def project() -> Project:
+    return init_project()
