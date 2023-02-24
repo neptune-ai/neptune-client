216bc6d2c7636866b9879663dea71a01cd5f40d9
Rafał Jankowski <rafal.jankowski@neptune.ai>
2023-02-24 13:57:25 +0100

    Added setup for neptune-client package (#1263)
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>

--- src/neptune/version.py
+++ src/neptune/version.py
@@ -16,23 +16,49 @@
 __all__ = ["version", "__version__"]
 
 import sys
+from typing import Optional
 
 from packaging.version import parse
 
-if sys.version_info >= (3, 8):
-    from importlib.metadata import (
-        PackageNotFoundError,
-        version,
-    )
-else:
-    from importlib_metadata import (
-        PackageNotFoundError,
-        version,
-    )
+from neptune.common.warnings import warn_once
 
-try:
-    __version__ = version("neptune")
-    version = parse(__version__)
-except PackageNotFoundError:
-    # package is not installed
-    pass
+if sys.version_info >= (3, 8):
+    from importlib.metadata import PackageNotFoundError
+    from importlib.metadata import version as version_parser
+else:
+    from importlib_metadata import PackageNotFoundError
+    from importlib_metadata import version as version_parser
+
+
+def check_version(package_name: str) -> Optional[str]:
+    try:
+        return version_parser(package_name)
+    except PackageNotFoundError:
+        # package is not installed
+        return None
+
+
+def detect_version() -> str:
+    neptune_version = check_version("neptune")
+    neptune_client_version = check_version("neptune-client")
+
+    if neptune_version is not None and neptune_client_version is not None:
+        raise RuntimeError(
+            "We've detected that the 'neptune' and 'neptune-client' packages are both installed. "
+            "Uninstall each of them and then install only the new 'neptune' package. For more, "
+            "see https://docs.neptune.ai/setup/upgrading/"
+        )
+    elif neptune_version is not None:
+        return neptune_version
+    elif neptune_client_version is not None:
+        warn_once(
+            "The 'neptune-client' package has been deprecated and will be removed in the future. Install "
+            "the 'neptune' package instead. For more, see https://docs.neptune.ai/setup/upgrading/"
+        )
+        return neptune_client_version
+
+    raise PackageNotFoundError("neptune")
+
+
+__version__ = detect_version()
+version = parse(__version__)
