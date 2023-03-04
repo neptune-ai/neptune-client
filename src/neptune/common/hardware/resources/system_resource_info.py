0fdbf5f658702834852b60d6716e255d900e2816
Rafal Jankowski <rafal.jankowski@neptune.ai>
2023-03-04 23:19:40 +0100

    Added type hints for system_resource_info

--- src/neptune/common/hardware/resources/system_resource_info.py
+++ src/neptune/common/hardware/resources/system_resource_info.py
@@ -13,43 +13,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from typing import List
 
 
-class SystemResourceInfo(object):
+class SystemResourceInfo:
     def __init__(
         self,
-        cpu_core_count,
-        memory_amount_bytes,
-        gpu_card_indices,
-        gpu_memory_amount_bytes,
-    ):
+        cpu_core_count: int,
+        memory_amount_bytes: int,
+        gpu_card_indices: List[int],
+        gpu_memory_amount_bytes: int,
+    ) -> None:
         self.__cpu_core_count = cpu_core_count
         self.__memory_amount_bytes = memory_amount_bytes
         self.__gpu_card_indices = gpu_card_indices
         self.__gpu_memory_amount_bytes = gpu_memory_amount_bytes
 
     @property
-    def cpu_core_count(self):
+    def cpu_core_count(self) -> int:
         return self.__cpu_core_count
 
     @property
-    def memory_amount_bytes(self):
+    def memory_amount_bytes(self) -> int:
         return self.__memory_amount_bytes
 
     @property
-    def gpu_card_count(self):
+    def gpu_card_count(self) -> int:
         return len(self.__gpu_card_indices)
 
     @property
-    def gpu_card_indices(self):
+    def gpu_card_indices(self) -> List[int]:
         return self.__gpu_card_indices
 
     @property
-    def gpu_memory_amount_bytes(self):
+    def gpu_memory_amount_bytes(self) -> int:
         return self.__gpu_memory_amount_bytes
 
-    def has_gpu(self):
+    def has_gpu(self) -> bool:
         return self.gpu_card_count > 0
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.__dict__)
