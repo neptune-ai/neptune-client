df234184017f3c71fe4cf184d750eb60086b019b
Sabine <sabine.nyholm@neptune.ai>
2023-03-23 15:36:46 +0200

    neptune package docstring, v1

--- src/neptune/__init__.py
+++ src/neptune/__init__.py
@@ -13,6 +13,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+"""Log and organize all your ML model metadata in a single place.
+
+The neptune package contains the functions needed to initialize Neptune objects.
+These help you track, store, and visualize metadata related to your model-training experiments.
+You can organize your metadata per experiment, model, model version, or project.
+
+Functions:
+    init_run()
+    init_model()
+    init_model_version()
+    init_project()
+
+Constants:
+    ANONYMOUS_API_TOKEN
+
+Runs
+----
+The run will track some things automatically during the execution of your model training
+script, such as hardware consumption, source code, and Git information. You can also
+assign any metadata to the run manually and organize it in a structure of your choosing.
+
+>>> run = neptune.init_run()
+>>> run["some/structure"] = some_metadata
+
+Model registry
+--------------
+Create a model object for a model you're working on:
+
+>>> model = neptune.init_model(key="KEY")
+>>> model["signature"].upload("signature.json")
+
+Create as many versions of the model as you need, tracking their metadata and
+lifecycles separately:
+
+>>> model_version = neptune.init_model_version(model="???-KEY")
+>>> model_version["dataset_version"].track_files("./data/train.csv")
+>>> model_version.change_stage("staging")
+
+Project-level metadata
+----------------------
+Initialize your entire Neptune project and log metadata on project-level:
+
+>>> project = neptune.init_project(project="ml-team/classification")
+>>> project["datasets"].upload("./data/")
+
+Anonymous logging
+-----------------
+To try out Neptune without registering, you can use the "ANONYMOUS_API_TOKEN" when
+initializing Neptune.
+
+>>> run = neptune.init_run(api_token=neptune.ANONYMOUS_API_TOKEN)
+
+---
+
+Learn more in the docs: https://docs.neptune.ai/api/neptune/
+"""
 __all__ = [
     "ANONYMOUS_API_TOKEN",
     "init_model",
