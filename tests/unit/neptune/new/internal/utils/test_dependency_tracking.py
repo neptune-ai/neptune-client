4a64b16951c85131f7a94f18b1448c3e6ed8bbf5
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-05-19 08:45:19 +0200

    add unit test for empty dependency string in infer mode

--- tests/unit/neptune/new/internal/utils/test_dependency_tracking.py
+++ tests/unit/neptune/new/internal/utils/test_dependency_tracking.py
@@ -19,6 +19,14 @@ class TestDependencyTracking(unittest.TestCase):
         mock_check_output.assert_called_once()
         mock_from_content.assert_called_once_with("some_dependency==1.0.0\n")
 
+    @patch("subprocess.check_output", return_value=b"")
+    @patch("neptune.types.File.from_content")
+    def test_infer_does_not_upload_empty_dependency_string(self, mock_from_content, mock_check_output):
+        InferDependenciesStrategy(run=MagicMock()).track_dependencies()
+
+        mock_check_output.assert_called_once()
+        mock_from_content.assert_not_called()
+
     @patch("neptune.handler.Handler.upload_files")
     def test_file_strategy_not_uploading_if_path_incorrect(self, mock_upload_files):
         FileDependenciesStrategy(run=MagicMock(), path="non-existent_file_path.txt").track_dependencies()
