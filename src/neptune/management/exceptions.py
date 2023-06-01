69848f149c71e8126e55dbda3c222284f10b1523
Rafał Jankowski <rafal.jankowski@neptune.ai>
2023-06-01 08:14:38 +0200

    Update src/neptune/management/exceptions.py
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>

--- src/neptune/management/exceptions.py
+++ src/neptune/management/exceptions.py
@@ -205,4 +205,8 @@ class ProjectPrivacyRestrictedException(ManagementOperationFailure):
 
 class ActiveProjectsLimitReachedException(ManagementOperationFailure):
     code = 26
-    description = "Limit of active projects reached. You can have up to {currentQuota} active projects simultaneously."
+    description = (
+        "Limit of active projects reached. You can have up to {currentQuota} active projects simultaneously. "
+        "To create a new project, you need to either archive an active project or increase the quota of active "
+        "projects in the workspace."
+    )
