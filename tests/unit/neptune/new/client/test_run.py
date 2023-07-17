5ef0792f3fea3bf9ec1a732f1ec58b4ed8ef0c95
AleksanderWWW <alwojnarowicz@gmail.com>
2023-07-17 11:22:47 +0200

    fix unittest by patching os.listdir

--- tests/unit/neptune/new/client/test_run.py
+++ tests/unit/neptune/new/client/test_run.py
@@ -193,6 +193,7 @@ class TestClientRun(AbstractExperimentTestMixin, unittest.TestCase):
         "neptune.internal.utils.os.path.abspath",
         new=lambda path: os.path.normpath(os.path.join("/home/user/main_dir", path)),
     )
+    @patch("neptune.internal.operation_processors.operation_storage.os.listdir", new=lambda path: [])
     def test_entrypoint_without_common_root(self):
         with init_run(mode="debug", source_files=["../*"]) as exp:
             self.assertEqual(exp["source_code/entrypoint"].fetch(), "/home/user/main_dir/main.py")
