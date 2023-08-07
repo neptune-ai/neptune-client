da07e0f0777edd8b1e27c60ce85857779be1dbb4
AleksanderWWW <alwojnarowicz@gmail.com>
2023-08-07 12:14:49 +0200

    apply pre commit

--- src/neptune/internal/operation_processors/sync_operation_processor.py
+++ src/neptune/internal/operation_processors/sync_operation_processor.py
@@ -45,7 +45,6 @@ class SyncOperationProcessor(OperationProcessor):
         process_path = f"exec-{now.timestamp()}-{now.strftime('%Y-%m-%d_%H.%M.%S.%f')}-{os.getpid()}"
         return get_container_dir(SYNC_DIRECTORY, container_id, container_type, process_path)
 
-
     def enqueue_operation(self, op: Operation, *, wait: bool) -> None:
         _, errors = self._backend.execute_operations(
             container_id=self._container_id,
