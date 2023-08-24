2528134b06d8325b0826a20a5d484afc978d0a68
AleksanderWWW <alwojnarowicz@gmail.com>
2023-08-24 19:23:38 +0200

    apply pre-commit

--- src/neptune/internal/types/stringify_value.py
+++ src/neptune/internal/types/stringify_value.py
@@ -44,7 +44,12 @@ class StringifyValue:
     def __init__(self, value: Any):
         # check if it's an integer outside 32bit range and cast it to float
         if isinstance(value, int) and (value > MAX_32_BIT_INT or value < MIN_32_BIT_INT):
-            logger.info("Value '%d' is outside the range of 32-bit integers ('%d' to '%d') and will be logged as float", value, MIN_32_BIT_INT, MAX_32_BIT_INT)
+            logger.info(
+                "Value '%d' is outside the range of 32-bit integers ('%d' to '%d') and will be logged as float",
+                value,
+                MIN_32_BIT_INT,
+                MAX_32_BIT_INT,
+            )
             value = float(value)
 
         self.__value = value
