f785992e54d74c025b4b9f0425141f53229471da
Rafal Jankowski <rafal.jankowski@neptune.ai>
2023-10-06 17:50:11 +0200

    Better coverage

--- tests/unit/neptune/new/internal/preprocessor/test_operations_preprocessor.py
+++ tests/unit/neptune/new/internal/preprocessor/test_operations_preprocessor.py
@@ -13,15 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+import datetime
 import uuid
 
 from neptune.exceptions import MetadataInconsistency
 from neptune.internal.operation import (
     AddStrings,
     AssignFloat,
+    AssignBool,
+    AssignDatetime,
     AssignInt,
     AssignString,
+    AssignArtifact,
+    UploadFile,
+    UploadFileContent,
+    ClearArtifact,
     ClearFloatLog,
     ClearImageLog,
     ClearStringSet,
@@ -33,6 +39,8 @@ from neptune.internal.operation import (
     RemoveStrings,
     TrackFilesToArtifact,
     UploadFileSet,
+    ClearStringLog,
+    DeleteFiles,
 )
 from neptune.internal.preprocessor.operations_preprocessor import OperationsPreprocessor
 
@@ -53,10 +61,15 @@ def test_delete_attribute():
         DeleteAttribute(["b"]),
         AssignString(["c"], "2"),
         AssignString(["c"], "3"),
+        AssignInt(["d"], 4),
+        AssignInt(["d"], 5),
+        AssignArtifact(["e"], "123"),
         DeleteAttribute(["c"]),
         DeleteAttribute(["d"]),
-        DeleteAttribute(["d"]),
         DeleteAttribute(["e"]),
+        DeleteAttribute(["f"]),
+        DeleteAttribute(["f"]),
+        DeleteAttribute(["f"]),
     ]
 
     # when
@@ -72,8 +85,11 @@ def test_delete_attribute():
         DeleteAttribute(["b"]),
         AssignString(["c"], "3"),
         DeleteAttribute(["c"]),
+        AssignInt(["d"], 5),
         DeleteAttribute(["d"]),
+        AssignArtifact(["e"], "123"),
         DeleteAttribute(["e"]),
+        DeleteAttribute(["f"]),
     ] == result.other_operations
     assert [] == result.errors
     assert len(operations) == processor.processed_ops_count
@@ -95,6 +111,12 @@ def test_assign():
         AssignFloat(["e"], 5),
         AssignFloat(["e"], 10),
         AssignFloat(["e"], 33),
+        AssignBool(["f"], False),
+        AssignBool(["f"], True),
+        AssignDatetime(["g"], datetime.datetime(1879, 3, 14)),
+        AssignDatetime(["g"], datetime.datetime(1846, 9, 23)),
+        UploadFile(["h"], "123"),
+        UploadFileContent(["i"], "123", "123"),
     ]
 
     # when
@@ -102,7 +124,10 @@ def test_assign():
 
     # then
     result = processor.get_operations()
-    assert [] == result.upload_operations
+    assert [
+        UploadFile(["h"], "123"),
+        UploadFileContent(["i"], "123", "123"),
+    ] == result.upload_operations
     assert [] == result.artifact_operations
     assert [
         AssignFloat(["a"], 1),
@@ -113,12 +138,14 @@ def test_assign():
         AssignFloat(["c"], 3),
         AssignString(["d"], "44"),
         AssignFloat(["e"], 33),
+        AssignBool(["f"], True),
+        AssignDatetime(["g"], datetime.datetime(1846, 9, 23)),
     ] == result.other_operations
     assert [
         MetadataInconsistency("Cannot perform AssignString operation on c: Attribute is not a String")
     ] == result.errors
     assert len(operations) == processor.processed_ops_count
-    assert result.other_operations == result.all_operations()
+    assert result.upload_operations + result.other_operations == result.all_operations()
 
 
 def test_series():
@@ -132,6 +159,7 @@ def test_series():
         LogStrings(["a"], [SLog("111", 3, 4)]),
         DeleteAttribute(["b"]),
         LogStrings(["b"], [SLog("222", None, 6)]),
+        ClearStringLog(["b"]),
         LogFloats(["c"], [FLog(1, 2, 3)]),
         LogFloats(["c"], [FLog(10, 20, 30), FLog(100, 200, 300)]),
         LogStrings(["d"], [SLog("4", 111, 222)]),
@@ -152,6 +180,8 @@ def test_series():
         ConfigFloatSeries(["i"], min=7, max=70, unit="%"),
         ClearFloatLog(["i"]),
         LogFloats(["i"], [FLog(10, 20, 30), FLog(100, 200, 300)]),
+        AssignInt(["j"], 2),
+        ConfigFloatSeries(["j"], min=0, max=100, unit="%"),
     ]
 
     # when
@@ -166,7 +196,7 @@ def test_series():
         DeleteAttribute(["a"]),
         LogStrings(["a"], [FLog("111", 3, 4)]),
         DeleteAttribute(["b"]),
-        LogStrings(["b"], [SLog("222", None, 6)]),
+        ClearStringLog(path=['b']),
         LogFloats(["c"], [FLog(1, 2, 3), FLog(10, 20, 30), FLog(100, 200, 300)]),
         LogStrings(["d"], [SLog("4", 111, 222)]),
         ClearFloatLog(["e"]),
@@ -186,9 +216,11 @@ def test_series():
         ClearFloatLog(["i"]),
         LogFloats(["i"], [FLog(10, 20, 30), FLog(100, 200, 300)]),
         ConfigFloatSeries(["i"], min=7, max=70, unit="%"),
+        AssignInt(["j"], 2),
     ] == result.other_operations
     assert [
-        MetadataInconsistency("Cannot perform LogFloats operation on h: Attribute is not a Float Series")
+        MetadataInconsistency("Cannot perform LogFloats operation on h: Attribute is not a Float Series"),
+        MetadataInconsistency("Cannot perform ConfigFloatSeries operation on j: Attribute is not a Float Series"),
     ] == result.errors
     assert len(operations) == processor.processed_ops_count
     assert result.other_operations == result.all_operations()
@@ -269,6 +301,7 @@ def test_file_set():
         UploadFileSet(["d"], ["hhh", "gij"], reset=False),
         AssignFloat(["e"], 5),
         UploadFileSet(["e"], [""], reset=False),
+        DeleteFiles(["d"], {"hhh"}),
     ]
 
     # when
@@ -286,6 +319,7 @@ def test_file_set():
     ]
     assert [] == result.artifact_operations
     assert [
+        DeleteFiles(["d"], {"hhh"}),
         AssignFloat(["e"], 5),
     ] == result.other_operations
     assert [
@@ -342,15 +376,17 @@ def test_artifacts():
         TrackFilesToArtifact(["b"], project_uuid, [("dir1/", None)]),
         TrackFilesToArtifact(["b"], project_uuid, [("dir2/dir3/", "dir2/")]),
         TrackFilesToArtifact(["b"], project_uuid, [("dir4/dir5/", "dir4/")]),
-        AssignFloat(["c"], 5),
-        TrackFilesToArtifact(["c"], project_uuid, [("dir1/", None)]),
-        TrackFilesToArtifact(["d"], project_uuid, [("dir2/dir3/", "dir2/")]),
-        TrackFilesToArtifact(["d"], project_uuid, [("dir4/", None)]),
-        TrackFilesToArtifact(["e"], project_uuid, [("dir1/", None)]),
+        TrackFilesToArtifact(["c"], project_uuid, [("dir4/dir5/", "dir4/")]),
+        AssignFloat(["d"], 5),
+        ClearArtifact(["c"]),
+        TrackFilesToArtifact(["d"], project_uuid, [("dir1/", None)]),
         TrackFilesToArtifact(["e"], project_uuid, [("dir2/dir3/", "dir2/")]),
+        TrackFilesToArtifact(["e"], project_uuid, [("dir4/", None)]),
         TrackFilesToArtifact(["f"], project_uuid, [("dir1/", None)]),
         TrackFilesToArtifact(["f"], project_uuid, [("dir2/dir3/", "dir2/")]),
-        TrackFilesToArtifact(["f"], project_uuid, [("dir4/", None)]),
+        TrackFilesToArtifact(["g"], project_uuid, [("dir1/", None)]),
+        TrackFilesToArtifact(["g"], project_uuid, [("dir2/dir3/", "dir2/")]),
+        TrackFilesToArtifact(["g"], project_uuid, [("dir4/", None)]),
         TrackFilesToArtifact(["a"], project_uuid, [("dir1/", None)]),
     ]
 
@@ -367,24 +403,21 @@ def test_artifacts():
             project_uuid,
             [("dir1/", None), ("dir2/dir3/", "dir2/"), ("dir4/dir5/", "dir4/")],
         ),
-        TrackFilesToArtifact(["d"], project_uuid, [("dir2/dir3/", "dir2/"), ("dir4/", None)]),
-        TrackFilesToArtifact(["e"], project_uuid, [("dir1/", None), ("dir2/dir3/", "dir2/")]),
+        TrackFilesToArtifact(["e"], project_uuid, [("dir2/dir3/", "dir2/"), ("dir4/", None)]),
+        TrackFilesToArtifact(["f"], project_uuid, [("dir1/", None), ("dir2/dir3/", "dir2/")]),
         TrackFilesToArtifact(
-            ["f"],
+            ["g"],
             project_uuid,
             [("dir1/", None), ("dir2/dir3/", "dir2/"), ("dir4/", None)],
         ),
     ] == result.artifact_operations
     assert [
-        DeleteAttribute(path=['a']),
-        AssignFloat(path=['c'], value=5)
-    ] == result.other_operations
-    assert [
         DeleteAttribute(["a"]),
-        AssignFloat(["c"], 5),
+        ClearArtifact(["c"]),
+        AssignFloat(["d"], 5),
     ] == result.other_operations
     assert [
-        MetadataInconsistency("Cannot perform TrackFilesToArtifact operation on c: Attribute is not a Artifact"),
+        MetadataInconsistency("Cannot perform TrackFilesToArtifact operation on d: Attribute is not a Artifact"),
     ] == result.errors
     assert len(operations) - 1 == processor.processed_ops_count
     assert result.artifact_operations + result.other_operations == result.all_operations()
