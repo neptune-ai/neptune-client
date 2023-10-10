2b3c9f4ec23a67a38f2fd1a67ec62cc6640384bb
Patryk Gala <patryk.gala@neptune.ai>
2023-10-10 10:06:03 +0200

    Revert "Upgraded performance of sending series data to Neptune (#1483)"
    This reverts commit 8ed5e05326fcb64dd37389d5d80026f728dc6384.

--- tests/unit/neptune/new/cli/utils.py
+++ tests/unit/neptune/new/cli/utils.py
@@ -26,8 +26,8 @@ from neptune.constants import (
 from neptune.exceptions import MetadataContainerNotFound
 from neptune.internal.backends.api_model import ApiExperiment
 from neptune.internal.container_type import ContainerType
-from neptune.internal.queue.disk_queue import DiskQueue
-from neptune.internal.queue.sync_offset_file import SyncOffsetFile
+from neptune.internal.disk_queue import DiskQueue
+from neptune.internal.utils.sync_offset_file import SyncOffsetFile
 from tests.unit.neptune.new.utils.api_experiments_factory import (
     api_metadata_container,
     api_run,
