b950898f1650bc7361648889b832fe3257e5a99a
Patryk Gala <patryk.gala@neptune.ai>
2023-10-10 16:20:36 +0200

    chore: warn_once log up to 10k messages

--- src/neptune/common/warnings.py
+++ src/neptune/common/warnings.py
@@ -21,6 +21,7 @@ __all__ = [
     "NeptuneUnsupportedType",
 ]
 
+import hashlib
 import os
 import traceback
 import warnings
@@ -55,16 +56,19 @@ def get_user_code_stack_level():
 
 
 def warn_once(message: str, *, exception: type(Exception) = None):
-    if exception is None:
-        exception = NeptuneDeprecationWarning
+    if len(warned_once) < 1_000:
+        if exception is None:
+            exception = NeptuneDeprecationWarning
 
-    if message not in warned_once:
-        warnings.warn(
-            message=message,
-            category=exception,
-            stacklevel=get_user_code_stack_level(),
-        )
-        warned_once.add(message)
+        message_hash = hashlib.sha256(message.encode("utf-8")).hexdigest()
+
+        if message_hash not in warned_once:
+            warnings.warn(
+                message=message,
+                category=exception,
+                stacklevel=get_user_code_stack_level(),
+            )
+            warned_once.add(message_hash)
 
 
 def warn_about_unsupported_type(type_str: str):
