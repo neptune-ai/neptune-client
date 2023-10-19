a58a1e77567bffada1815b019b0db5520d198c3e
Rafal Jankowski <rafal.jankowski@neptune.ai>
2023-10-19 13:24:35 +0200

    No experimental mode part 2

--- src/neptune/internal/backends/factory.py
+++ src/neptune/internal/backends/factory.py
@@ -27,7 +27,7 @@ from .offline_neptune_backend import OfflineNeptuneBackend
 
 
 def get_backend(mode: Mode, api_token: Optional[str] = None, proxies: Optional[dict] = None) -> NeptuneBackend:
-    if mode in (Mode.ASYNC, Mode.EXPERIMENTAL):
+    if mode == Mode.ASYNC:
         return HostedNeptuneBackend(credentials=Credentials.from_token(api_token=api_token), proxies=proxies)
     elif mode == Mode.SYNC:
         return HostedNeptuneBackend(credentials=Credentials.from_token(api_token=api_token), proxies=proxies)
