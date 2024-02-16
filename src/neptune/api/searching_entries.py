d4ac35030f6fb73bebf2a6f1d17e91d91a7a7180
AleksanderWWW <alwojnarowicz@gmail.com>
2024-02-16 13:53:53 +0100

    Add missing args to get_single_page call (fix e2e)

--- src/neptune/api/searching_entries.py
+++ src/neptune/api/searching_entries.py
@@ -185,6 +185,10 @@ def iter_over_pages(
     total = get_single_page(
         limit=0,
         offset=0,
+        sort_by=sort_by,
+        sort_by_column_type=sort_by_column_type,
+        ascending=ascending,
+        searching_after=None,
         **kwargs,
     ).get("matchingItemCount", 0)
 
