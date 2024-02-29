d1061cc44dc0dac15767d6d0599460ff4526fca4
AleksanderWWW <alwojnarowicz@gmail.com>
2024-02-29 08:58:07 +0100

    scale images to [0,1] range (#1664)

--- tests/unit/neptune/new/internal/utils/test_images.py
+++ tests/unit/neptune/new/internal/utils/test_images.py
@@ -39,6 +39,7 @@ from neptune.common.utils import (
     IS_WINDOWS,
 )
 from neptune.internal.utils.images import (
+    _scale_array,
     get_html_content,
     get_image_content,
 )
@@ -75,23 +76,13 @@ class TestImage(unittest.TestCase):
     def test_get_image_content_from_3d_grayscale_array(self):
         # given
         image_array = numpy.array([[[1], [0]], [[-3], [4]], [[5], [6]]])
-        expected_array = numpy.array([[1, 0], [-3, 4], [5, 6]]) * 255
+        expected_array = numpy.array([[1, 0], [-3, 4], [5, 6]])
         expected_image = Image.fromarray(expected_array.astype(numpy.uint8))
 
         # when
-        _log = partial(format_log, "WARNING")
 
         # expect
-        stdout = io.StringIO()
-        with contextlib.redirect_stdout(stdout):
-            self.assertEqual(get_image_content(image_array), self._encode_pil_image(expected_image))
-        self.assertEqual(
-            stdout.getvalue(),
-            _log(
-                "The smallest value in the array is -3 and the largest value in the array is 6."
-                " To be interpreted as colors correctly values in the array need to be in the [0, 1] range.\n",
-            ),
-        )
+        self.assertEqual(get_image_content(image_array), self._encode_pil_image(expected_image))
 
     def test_get_image_content_from_rgb_array(self):
         # given
@@ -292,3 +283,46 @@ class TestImage(unittest.TestCase):
             return numpy.random.rand(w, h, d)
         else:
             return numpy.random.rand(w, h)
+
+
+def test_scale_array_when_array_already_scaled():
+    # given
+    arr = numpy.array([[123, 32], [255, 0]])
+
+    # when
+    result = _scale_array(arr)
+
+    # then
+    assert numpy.all(arr == result)
+
+
+def test_scale_array_when_array_not_scaled():
+    # given
+    arr = numpy.array([[0.3, 0], [0.5, 1]])
+
+    # when
+    result = _scale_array(arr)
+    expected = numpy.array([[76.5, 0.0], [127.5, 255.0]])
+
+    # then
+    assert numpy.all(expected == result)
+
+
+def test_scale_array_incorrect_range():
+    # given
+    arr = numpy.array([[-12, 7], [300, 0]])
+
+    # when
+    _log = partial(format_log, "WARNING")
+
+    stdout = io.StringIO()
+    with contextlib.redirect_stdout(stdout):
+        result = _scale_array(arr)
+
+    # then
+    assert numpy.all(arr == result)  # returned original array
+
+    assert stdout.getvalue() == _log(
+        "Image data is in range [-12, 300]. To be interpreted as colors "
+        "correctly values in the array need to be in the [0, 255] or [0.0, 1.0] range.\n",
+    )
