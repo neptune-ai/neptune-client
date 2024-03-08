2e859eb2d029d5b889323c52be8038eafa11b310
AleksanderWWW <alwojnarowicz@gmail.com>
2024-03-08 10:55:01 +0100

    fix?

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,14 +1,11 @@
 import logging
 
-import pytest
-
 from neptune.internal.utils.logger import (
     NEPTUNE_LOGGER_NAME,
     NEPTUNE_NO_PREFIX_LOGGER_NAME,
 )
 
 
-@pytest.fixture(autouse=True, scope="session")
-def set_logging_level():
+def pytest_sessionstart(session):
     logging.getLogger(NEPTUNE_LOGGER_NAME).setLevel(logging.DEBUG)
     logging.getLogger(NEPTUNE_NO_PREFIX_LOGGER_NAME).setLevel(logging.DEBUG)
