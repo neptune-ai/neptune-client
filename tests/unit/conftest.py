d0d88dbca4fcb6364d1de330413fbeef15eb008b
AleksanderWWW <alwojnarowicz@gmail.com>
2024-03-08 10:28:35 +0100

    use fixture

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,11 +1,14 @@
 import logging
 
+import pytest
+
 from neptune.internal.utils.logger import (
     NEPTUNE_LOGGER_NAME,
     NEPTUNE_NO_PREFIX_LOGGER_NAME,
 )
 
 
-def pytest_sessionstart(session):
+@pytest.fixture(autouse=True)
+def set_logging_level():
     logging.getLogger(NEPTUNE_LOGGER_NAME).setLevel(logging.DEBUG)
     logging.getLogger(NEPTUNE_NO_PREFIX_LOGGER_NAME).setLevel(logging.DEBUG)
