8d8009568cc5a6e071aea4abb43445c26eb54fb0
AleksanderWWW <alwojnarowicz@gmail.com>
2024-03-12 15:40:51 +0100

    Delete legacy package (#1685)

--- src/neptune/integrations/utils.py
+++ src/neptune/integrations/utils.py
@@ -13,21 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-__all__ = ["expect_not_an_experiment", "join_paths", "verify_type", "RunType"]
+__all__ = ["join_paths", "verify_type", "RunType"]
 
 from typing import Union
 
 from neptune import Run
-from neptune.common.experiments import LegacyExperiment as Experiment
-from neptune.exceptions import NeptuneLegacyIncompatibilityException
 from neptune.handler import Handler
 from neptune.internal.utils import verify_type
 from neptune.internal.utils.paths import join_paths
-
-
-def expect_not_an_experiment(run: Run):
-    if isinstance(run, Experiment):
-        raise NeptuneLegacyIncompatibilityException()
-
 
 RunType = Union[Run, Handler]
