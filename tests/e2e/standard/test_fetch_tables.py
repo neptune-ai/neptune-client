aae1f79dfa8586d0928f7984242bc625a48d97da
Szymon Sadkowski <szymon.sadkowski@neptune.ai>
2024-03-20 12:57:24 +0100

    expect tests to fail instead of skipping them

--- tests/e2e/standard/test_fetch_tables.py
+++ tests/e2e/standard/test_fetch_tables.py
@@ -60,7 +60,7 @@ class TestFetchTable(BaseE2ETest):
         assert len(runs) == 1
         assert runs[0].get_attribute_value("sys/id") == run_id1
 
-    @pytest.mark.xfail(reason="Model is not supported", raises=NeptuneUnsupportedFunctionalityException)
+    @pytest.mark.xfail(reason="Model is not supported", strict=True, raises=NeptuneUnsupportedFunctionalityException)
     @pytest.mark.parametrize("with_query", [True, False])
     @pytest.mark.parametrize("container_fn_scope", ["model"], indirect=True)
     def test_fetch_model_versions_with_correct_ids(self, container_fn_scope: Model, environment, with_query: bool):
@@ -158,7 +158,7 @@ class TestFetchTable(BaseE2ETest):
 
         self._test_fetch_from_container(init_run, get_runs_as_rows)
 
-    @pytest.mark.xfail(reason="Model is not supported", raises=NeptuneUnsupportedFunctionalityException)
+    @pytest.mark.xfail(reason="Model is not supported", strict=True, raises=NeptuneUnsupportedFunctionalityException)
     def test_fetch_models_table(self, environment, project):
         def init_run():
             return neptune.init_model(project=environment.project, key=a_key())
@@ -174,7 +174,7 @@ class TestFetchTable(BaseE2ETest):
             pytest.param(
                 "model",
                 marks=pytest.mark.xfail(
-                    reason="Model is not supported", raises=NeptuneUnsupportedFunctionalityException
+                    reason="Model is not supported", strict=True, raises=NeptuneUnsupportedFunctionalityException
                 ),
             )
         ],
@@ -403,7 +403,7 @@ class TestFetchTable(BaseE2ETest):
         with pytest.raises(NeptuneInvalidQueryException):
             next(iter(runs_table))
 
-    @pytest.mark.xfail(reason="Model is not supported", raises=NeptuneUnsupportedFunctionalityException)
+    @pytest.mark.xfail(reason="Model is not supported", strict=True, raises=NeptuneUnsupportedFunctionalityException)
     @pytest.mark.skipif(IS_MACOS, reason="MacOS behaves strangely on github actions")
     def test_fetch_models_raw_query_trashed(self, environment, project):
         # given
