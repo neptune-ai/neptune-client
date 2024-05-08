bf24b32207e445f96bcbe08da22c04179ead6b6a
AleksanderWWW <alwojnarowicz@gmail.com>
2024-05-08 15:45:04 +0200

    improve test structure

--- tests/unit/neptune/new/core/operation_processors/test_async_operation_processor.py
+++ tests/unit/neptune/new/core/operation_processors/test_async_operation_processor.py
@@ -1,3 +1,19 @@
+#
+# Copyright (c) 2024, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import random
 import threading
 from pathlib import Path
@@ -15,11 +31,14 @@ from neptune.core.typing.id_formats import UniqueId
 @patch("neptune.core.operation_processors.async_operation_processor.MetadataFile", new=Mock)
 class TestAsyncOperationProcessorInit:
     def test_resources(self):
+        # given
         processor = AsyncOperationProcessor(
             container_id=UniqueId("test_id"),
             container_type=random.choice(list(ContainerType)),
             lock=threading.RLock(),
         )
+
+        # then
         assert processor.resources == (processor._metadata_file, processor._operation_storage, processor._queue)
 
     @patch(
@@ -30,6 +49,7 @@ class TestAsyncOperationProcessorInit:
     @patch("neptune.core.components.operation_storage.os.makedirs", new=lambda *_, **__: None)
     @patch("neptune.core.operation_processors.async_operation_processor.DiskQueue", new=Mock)
     def test_data_path(self, mock_get_container_full_path):
+        # given
         test_cases = [
             {"data_path": Path("test_data_path"), "expected": Path("test_data_path")},
             {"data_path": None, "expected": Path("mock_path")},
@@ -43,6 +63,7 @@ class TestAsyncOperationProcessorInit:
                 lock=threading.RLock(),
                 data_path=test_case["data_path"],
             )
+            # then
             assert processor.data_path == test_case["expected"]
 
             if test_case["data_path"] is None:
