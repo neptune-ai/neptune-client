c0b98ef92b593ec867f6bb20852391fdc7d670c9
AleksanderWWW <alwojnarowicz@gmail.com>
2024-05-20 12:41:30 +0200

    Init code for SyncOperationProcessor (#1779)

--- src/neptune/core/typing/id_formats.py
+++ src/neptune/core/typing/id_formats.py
@@ -13,12 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-__all__ = ["UniqueId", "SysId", "QualifiedName"]
+__all__ = ["UniqueId", "QualifiedName", "CustomId"]
 
 from typing import NewType
 
 UniqueId = NewType("UniqueId", str)
 
-SysId = NewType("SysId", str)
-
 QualifiedName = NewType("QualifiedName", str)
+
+CustomId = NewType("CustomId", str)
